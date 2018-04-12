fb251e1beae0e412af3ac5e6d1d4595ac6d317ac
prathap <sivaprathap.konduru@gmail.com>
2018-04-12 16:22:11 +0530

    problem with the table striping

---
+++ data-table/src/app/app.component.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ flight = [
+{
+"name": "Jet Airways",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-11T01:58:13.768Z",
+"arrivalTime": "2018-04-11T02:58:13.768Z"
+},
+{
+"name": "Air India",
+"bordingPoint": "Mumbai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-12T01:58:13.768Z",
+"arrivalTime": "2018-04-12T02:58:13.768Z"
+},
+{
+"name": "IndiGo",
+"bordingPoint": "Bangalore",
+"destinationPoint": "Kolkata",
+"departureTime": "2018-04-15T01:58:13.768Z",
+"arrivalTime": "2018-04-15T02:58:13.768Z"
+},
+{
+"name": "SpiceJet",
+"bordingPoint": "Delhi",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-14T01:58:13.768Z",
+"arrivalTime": "2018-04-14T02:58:13.768Z"
+},
+{
+"name": "GoAir",
+"bordingPoint": "Kolkata",
+"destinationPoint": "Chennai",
+"departureTime": "2018-04-13T01:58:13.768Z",
+"arrivalTime": "2018-04-13T02:58:13.768Z"
+},
+{
+"name": "Vistara",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-17T01:58:13.768Z",
+"arrivalTime": "2018-04-17T02:58:13.768Z"
+},
+{
+"name": "Zoom Air",
+"bordingPoint": "Kolkata",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-19T01:58:13.768Z",
+"arrivalTime": "2018-04-19T02:58:13.768Z"
+},
+{
+"name": "Blue Dart Aviation",
+"bordingPoint": "Chennai",
+"destinationPoint": "Mumbai",
+"departureTime": "2018-04-13T01:58:13.768Z",
+"arrivalTime": "2018-04-13T02:58:13.768Z"
+},
+{
+"name": "Deccan Charters",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-17T01:58:13.768Z",
+"arrivalTime": "2018-04-17T02:58:13.768Z"
+},
+{
+"name": "TajAir",
+"bordingPoint": "Chennai",
+"destinationPoint": "Delhi",
+"departureTime": "2018-04-15T01:58:13.768Z",
+"arrivalTime": "2018-04-15T02:58:13.768Z"
+},
+{
+"name": "Titan Aviation",
+"bordingPoint": "Chennai",
+"destinationPoint": "Bangalore",
+"departureTime": "2018-04-11T01:58:13.768Z",
+"arrivalTime": "2018-04-11T02:58:13.768Z"
+}
+]
+}
